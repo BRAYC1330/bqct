32fcb5e9a2d684c8458637b3e191efa2ff8d2213
BRAYC1330 <256725968+BRAYC1330@users.noreply.github.com>
2026-05-25 16:50:33 +0000

    Update build_content.py

--- build_content.py
+++ build_content.py
@@ -9,7 +9,7 @@ import io
 import bsky
 import prompt_engine
 logger = logging.getLogger(__name__)
-SIG_DIGEST = "\n\nQwen | Chainbase TOPS " + config.SIGNATURE_ICONS
+SIG_DIGEST = "\n\nQwen | Chainbase crypto TOPS " + config.SIGNATURE_ICONS
 SIG_TAVILY = "\n\nQwen | Tavily"
 SIG_CHAINBASE = "\n\nQwen | Chainbase"
 SIG_DEFAULT = "\n\nQwen"
